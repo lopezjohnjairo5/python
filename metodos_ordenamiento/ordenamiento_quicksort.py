c945c2d112d89ef647ccc4cbcbbdd439cd0a77ac
lopezjohnjairo5 <72105633+lopezjohnjairo5@users.noreply.github.com>
2022-10-18 22:43:41 -0500

    Update ordenamiento_quicksort.py

--- metodos_ordenamiento/ordenamiento_quicksort.py
+++ metodos_ordenamiento/ordenamiento_quicksort.py
@@ -7,4 +7,35 @@ quicksort
 
 basado en recursividad
 """
+import random as rd
 
+def quicksort(l):
+	"""
+	funcion encargada de ordenar los 
+	elementos de la lista pasada por 
+	parametro
+	"""
+	left = []
+	right = []
+	pivot = 0
+
+	if len(l) < 2:
+		return l
+
+	else:
+		for i in range(1,len(l)):
+			pivot = l[0]
+			if l[i] < pivot:
+				left.append(l[i])
+			else:	
+				right.append(l[i])
+
+		left.append(pivot)
+		
+		return quicksort(left) + quicksort(right)
+
+
+if __name__ == '__main__':
+	l1 = [rd.randint(0,100) for i in range(0,10)]
+	result = quicksort(l1)
+	print("Original:\n{0}\nResultado asc:\n{1}\nResultado desc:\n{2}".format(l1,result,result[::-1]))
